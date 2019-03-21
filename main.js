b883605f0a0348dadc1d8b78eb4e4365137302ba
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2019-03-21 15:06:13 -0600

    fixed particles to use image

---
+++ main.js
@@ -0,0 +1,9 @@
+/*Image source array*/
+let img_src = ['assets/twitter.png', 'assets/github.png', 'assets/linkedin.png'];
+let particles = 'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js'
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('particles-js', 'particles.json', function () {
+//   console.log('callback - particles.js config loaded');
+// });
+
